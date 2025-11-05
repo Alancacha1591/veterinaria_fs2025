5b302ebf51cb452401e0a21c8a523e8aa4c6747a
Alancacha1591 <alancacha1591@gmail.com>
2025-11-05 12:06:44 -0600

    Titulo Modificado por Alan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Veterinaria 2025 - Cambios crados por Alan</title>
+        <title>Veterinaria 2025 - Rama Alan</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
